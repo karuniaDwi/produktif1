f2f1c52b8ac05902288a9911a72bbe91effb9e64
karuniaDwi <karuniadwi130@gmail.com>
2024-01-31 12:16:49 +0700

    First Upload

---
+++ tugas1.php
@@ -0,0 +1,8 @@
+<?php
+ $string = "100";
+ $numerik = 90;
+ $perjumlahannya = $string + $numerik;
+ echo "nilai string = $string <br>";
+ echo "nlai numerik = $numerik <br>";
+ echo "hasil dari penjumlahan tersebut adalah = $perjumlahannya"; 
+?>
---
+++ tugas2.php
@@ -0,0 +1,10 @@
+<?php
+ $angka1 = 50;
+ $angka2 = 40;
+ echo "nilai bilangan1 = $angka1 <br>";
+ echo "nilai bilangan2 = $angka2 <br>";
+ echo "pertama jika menggunakan pembagian dengan operator / <br>";
+ echo "contoh, $angka1 dibagi $angka2 maka akan mendapatkan hasil =  ", ($angka1/$angka2),"<br>";
+ echo "tetapi, jika menggunakan pembagian dengan operator % (modulo) <br>";
+ echo "contoh, $angka1 dimodulos $angka2 maka akan mendapatkan hasil = ". ($angka1%$angka2);
+?>
